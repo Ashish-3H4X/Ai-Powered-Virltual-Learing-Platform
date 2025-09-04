f94f46c2d42e3525603a2f5f1506262b6fa830b9
Mr Rahul <ashish227087@gmail.com>
2025-09-04 11:57:46 +0530

    cloudinary and multer setup

--- Backend/controller/userContoller.js
+++ Backend/controller/userContoller.js
@@ -1,5 +1,6 @@
 import { response } from "express"
 import User from "../model/userModel.js"
+import uploadOnCloudinary from "../config/cloudinary.js"
 
 
 export const getCurrentUser = async (req , res) => {
@@ -11,5 +12,28 @@ export const getCurrentUser = async (req , res) => {
      return res.status(200).json(user)
   } catch (error) {
       return res.status(500).json({ message: `GetCurrentUserError: ${error.message}` });
+  }
+}
+
+
+
+export const updateProfile = async(req,res)=>{
+  try {
+      const userId = req.userId
+    const {description , name } = req.body
+    let photoUrl
+    if(req.file){
+      photoUrl = await uploadOnCloudinary(req.file.path)
+
+    }
+    const user = await User.findByIdAndUpdate(userId , {name , description, photoUrl})
+    if (!user) {
+      return res.status(200).json({message:"User not Found"})
+      
+    }
+    return res.status(200).json(user)
+
+  } catch (error) {
+     return res.status(500).json({ message: `Update profile error : ${error.message}` }); 
   }
 }
--- Backend/route/userRoute.js
+++ Backend/route/userRoute.js
@@ -1,10 +1,12 @@
 import express from "express"
 import isAuth from "../middleware/isAuth.js"
-import { getCurrentUser } from "../controller/userContoller.js"
+import { getCurrentUser, updateProfile } from "../controller/userContoller.js"
+import upload from "../middleware/multer.js"
 
 const userRouter = express.Router()
 
 userRouter.get("/getcurrentuser", isAuth , getCurrentUser)
+userRouter.put("/profile", isAuth, upload.single("photoUrl"), updateProfile);
 
 
 
